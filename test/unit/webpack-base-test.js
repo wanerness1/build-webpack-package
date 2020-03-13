8bd39f13e4f8c0146dbc526e57b50ad5a854ce87
wanerness1 <waner19891117@163.com>
2020-03-14 02:15:45 +0800

    fix test case

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -7,7 +7,7 @@ describe('webpack.base.js test case',()=>{
     console.log(baseConfig);
     
     it('entry',()=>{
-        assert.equal(baseConfig.entry.index.indexOf('/build-webpack/test/smoke/template/src/index/index.js')>-1,true)
-        assert.equal(baseConfig.entry.search.indexOf('/build-webpack/test/smoke/template/src/search/index.js')>-1,true)
+        assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js')>-1,true)
+        assert.equal(baseConfig.entry.search.indexOf('/test/smoke/template/src/search/index.js')>-1,true)
     })
 })
